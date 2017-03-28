e5daff36583711aaa745b9ae9fdb4fae9eaf0418
Elias Becerra <eliashbf@gmail.com>
2017-03-28 13:38:44 -0300

    Agregar archivos de proyecto.

---
+++ Dibbi/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Dibbi
+{
+    [Activity(Label = "Dibbi", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
